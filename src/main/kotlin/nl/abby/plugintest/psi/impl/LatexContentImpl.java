9ee67029bbb71422e34248ebae8d1c34a0aa27e1
Abby Berkers <abbyberkers@gmail.com>
2019-04-13 20:50:32 +0200

    fix some error

--- src/main/kotlin/nl/abby/plugintest/psi/impl/LatexContentImpl.java
+++ src/main/kotlin/nl/abby/plugintest/psi/impl/LatexContentImpl.java
@@ -4,9 +4,10 @@ package nl.abby.plugintest.psi.impl;
 import com.intellij.extapi.psi.ASTWrapperPsiElement;
 import com.intellij.lang.ASTNode;
 import com.intellij.psi.PsiElementVisitor;
+import nl.abby.plugintest.psi.LatexContent;
+import nl.abby.plugintest.psi.LatexNoMathContent;
+import nl.abby.plugintest.psi.LatexVisitor;
 import org.jetbrains.annotations.NotNull;
-
-import static nl.abby.plugintest.psi.LatexTypes.*;
 
 public class LatexContentImpl extends ASTWrapperPsiElement implements LatexContent {
 
